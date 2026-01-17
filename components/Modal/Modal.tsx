2eb47f919762319ed1d084b4d514540920be228f
Nik <220475637+SvgNik@users.noreply.github.com>
2026-01-18 00:56:13 +0200

    change id, app, components

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import css from "./Modal.module.css";
 
@@ -8,34 +10,43 @@ interface ModalProps {
   isOpen: boolean;
 }
 
-const Modal = ({ children, onClose }: ModalProps) => {
-  const modalRoot = document.querySelector("#modal-root");
+const Modal = ({ children, onClose, isOpen }: ModalProps) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    setMounted(true);
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.code === "Escape") onClose();
     };
 
-    window.addEventListener("keydown", handleKeyDown);
-
-    document.body.style.overflow = "hidden";
+    if (isOpen) {
+      window.addEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "hidden";
+    }
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
       document.body.style.overflow = "unset";
     };
-  }, [onClose]);
+  }, [onClose, isOpen]);
 
-  const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === e.currentTarget) {
-      onClose();
-    }
-  };
+  if (!mounted) return null;
+  if (!isOpen) return null;
 
+  const modalRoot = document.getElementById("modal-root");
   if (!modalRoot) return null;
 
   return createPortal(
-    <div className={css.backdrop} onClick={handleBackdropClick}>
+    <div
+      className={css.backdrop}
+      onClick={(e) => {
+        if (e.target === e.currentTarget) onClose();
+      }}
+    >
       <div className={css.modal}>{children}</div>
     </div>,
     modalRoot,
